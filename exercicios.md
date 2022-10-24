7d0efb4175f093a80c3e8f684efa5c69d41f735d
Lucas Silva <lucas.devJ@outlook.com>
2022-10-24 15:41:26 -0300

    Inicio da leitura do livro

---
+++ exercicios.md
@@ -0,0 +1,13 @@
+### 1.1 - Suponha que voce tenha uma lista com 128 nomes e esteja fazendo uma pesquisa binária. Qual seria onúmero máximo de etapas que vc levaria para encontrar o nome desejado?
+
+* 128 -> 64 -> 32 -> 16 -> 8 -> 4 -> 2 -> 1 
+    * 7 etapas
+
+---
+### 1.2 - Suponha  que vc duplique o tamaho da lista. QUal seria o número máximo de etas agoras?
+
+
+#### 128 x 2  = 256 
+* 256 -> 128 -> 64 -> 32 -> 16 -> 8 -> 4 -> 2 -> 1 
+    * 8 etapas
+---
--- pequisa_binaria.py
+++ pequisa_binaria.py
@@ -1,19 +1,22 @@
 def pesquisa_binaria(lista, item):
-    baixo = 0
+    baixo = 0  # baixo  e alto acompanham a parte da lista que voce está proucurando
+    # baixo  e alto acompanham a parte da lista que voce está proucurando
     alto = len(lista) - 1
 
-    while baixo <= alto:
-        meio = (baixo + alto) / 2
+    while baixo <= alto:  # Entquanto ainda não conseguiu chegar a um único elemento
+        meio = (baixo + alto) / 2  # ...Verifica o elemento central.
         chute = lista[meio]
-        if chute == item:
+        if chute == item:  # Acha o item.
             return meio
-        if chute > item:
+        if chute > item:  # O chute foi muito alto
             alto = meio - 1
-        else:
+        else:  # O chute foi muito baixo
             baixo = meio + 1
-    return None
+    return None  # O item não existe
 
 
-minha_lista = [1, 3, 5, 7, 9]
-print (pesquisa_binaria(minha_lista, 3))
-print (pesquisa_binaria(minha_lista, -1))
+minha_lista = [1, 3, 5, 7, 9]  # vamos testá-lo
+# Lembre-se, as listas começam no 0. O  próximo endereço tem índice 1
+print(pesquisa_binaria(minha_lista, 3))
+# "None" significa nulo em python. Ele indica que o item não foi encontrado.
+print(pesquisa_binaria(minha_lista, -1))
